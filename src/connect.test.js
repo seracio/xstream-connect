628567538eeb1d58f6cd970f4dd3069c8e0eeb20
nicolasmondon <nicolasmondon@gmail.com>
2017-02-01 21:15:46 +0100

    issue #1: tests for connect, first draft

--- src/connect.test.js
+++ src/connect.test.js
@@ -1,33 +1,93 @@
+import _ from 'lodash/fp';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import xs from 'xstream';
 import connect from './connect';
 import Provider from './Provider';
 
+class MyComponent extends React.Component {
+  render(){
+    return <div>hello</div>;
+  }
+}
+
 test('connect: should return a function', () => {
-  expect('test').toBe('done');
+  expect(typeof connect(_.identity)).toBe('function');
 });
 
 test('connect: storeToPropsFunc param should be mandatory', () => {
-  expect('test').toBe('done');
+  expect(connect()).toThrow();
 });
 
 test('connect: storeToPropsFunc param should be a function', () => {
-  expect('test').toBe('done');
+  expect(connect('toto')).toThrow();
+});
+
+test('Connect component: should be a React component', () => {
+  expect(typeof connect(_.identity)(MyComponent)).toBe('function');
 });
 
 test('Connect component: should render false when state.go === false', () => {
-  expect('test').toBe('done');
+  const store = {none: xs.never()};
+  const MyComponentWrapped = connect(_.identity)(MyComponent);
+  class App extends React.Component {
+    render(){
+      return <MyComponentWrapped />;
+    }
+  }
+  const component = renderer.create(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
 
 test('Connect component: should only render the WrappedComponent when go === true', () => {
-  expect('test').toBe('done');
+  const store = {once: xs.of(1)};
+  const MyComponentWrapped = connect(_.identity)(MyComponent);
+  class App extends React.Component {
+    render(){
+      return <MyComponentWrapped />;
+    }
+  }
+  const component = renderer.create(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
 
 test('Connect component: fragment attribute should contains keys specified by the storeToPropsFunc parameter', () => {
-  expect('test').toBe('done');
+  const store = {hello$: xs.of('world')};
+  class MyOtherComponent extends React.Component {
+    render(){
+      return <div>{this.props.hello}</div>;
+    }
+  }
+  MyOtherComponent.propTypes = {
+    hello: React.PropTypes.string.isRequired
+  };
+  const MyComponentWrapped = connect(state => ({
+    hello: state.hello$
+  }))(MyOtherComponent);
+  class App extends React.Component {
+    render(){
+      return <MyComponentWrapped />;
+    }
+  }
+  const component = renderer.create(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
 
-test('Connect component: should set state when store values changed', () => {
-  expect('test').toBe('done');
+test.skip('Connect component: should set state when store values changed', () => {
+
 });
