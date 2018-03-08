e946c7c38b605ea6a3c1d3441a4fe410554d54ed
nicolasmondon <nicolasmondon@gmail.com>
2018-03-08 12:27:21 +0100

    sandboxing

--- .babelrc.js
+++ .babelrc.js
@@ -1,23 +1,38 @@
 const { BABEL_ENV, NODE_ENV } = process.env;
 const cjs = BABEL_ENV === 'cjs' || NODE_ENV === 'test';
 
+const presetsSandbox = [
+    '@babel/preset-flow',
+    '@babel/preset-react',
+    [
+        '@babel/preset-env',
+        {
+            targets: {
+                node: 'current'
+            }
+        }
+    ]
+];
+
+const presetsProduction = [
+    '@babel/preset-flow',
+    '@babel/preset-react',
+    [
+        '@babel/preset-env',
+        {
+            targets: {
+                browsers: ['last 2 versions', 'safari >= 7']
+            },
+            modules: false,
+            forceAllTransforms: true
+        }
+    ]
+];
+
 module.exports = {
     plugins: [
         '@babel/plugin-proposal-class-properties',
         '@babel/plugin-proposal-object-rest-spread'
     ],
-    presets: [
-        '@babel/preset-flow',
-        '@babel/preset-react',
-        [
-            '@babel/preset-env',
-            {
-                targets: {
-                    browsers: ['last 2 versions', 'safari >= 7']
-                },
-                modules: false,
-                forceAllTransforms: true
-            }
-        ]
-    ]
+    presets: BABEL_ENV === 'sandbox' ? presetsSandbox : presetsProduction
 };
--- src/sandbox.js
+++ src/sandbox.js
@@ -1,4 +1,23 @@
 // @flow
 import React from 'react';
 
-console.log(React.creatContext);
+const StoreContext = React.createContext(null);
+
+type PropsProvider = {
+    store: { [key: string]: any },
+    children: any
+};
+
+class Provider extends React.PureComponent<PropsProvider> {
+    render() {
+        <StoreContext.Provider value={this.props.store}>
+            {this.props.children}
+        </StoreContext.Provider>;
+    }
+}
+
+const connect = combinator => WrappedComponent => {
+    class Connect extends React.PureComponent<*> {}
+};
+
+console.log(StoreContext);
